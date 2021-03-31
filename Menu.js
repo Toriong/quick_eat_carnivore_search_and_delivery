8c00fbb5b688fbf27df23d99a53423bdfec9a7f9
Toriong <67920345+Toriong@users.noreply.github.com>
2021-03-30 20:39:20 -0500

    url parameter added
    Currently working on accessing the URL parameter. The program keeps on throwing an error when useParams() is called.

--- Menu.js
+++ Menu.js
@@ -1,33 +1,16 @@
-import React from 'react'
-import { useEffect } from 'react/cjs/react.development';
+import React, { useState } from 'react';
+import { useParams } from 'react-router-dom'
+
+
 
 const Menu = () => {
-    const meatList = require('./Meat-Shops.json');
-    useEffect(() => {
-        console.log(meatList);
-    }, [])
+    const restaurant = useParams();
+    console.log(restaurant);
     return (
         <div>
-            {/* title of the Menu page */}
-            <h1>Main Meats</h1>
-            {meatList.map((restaurant) => {
-                return (
-                    restaurant.main_meats.map((meat) => {
-                        return (
-                            <div>
-                                <h3>{meat.name}</h3>
-                                <h4>{meat.price}</h4>
-                                <div>
-                                    <img src={meat.image} alt={meat.alt} />
-                                </div>
-                            </div>
-                        );
-                    })
-                )
-            })}
-
+            <p>Hello world</p>
         </div>
     )
 }
 
-export default Menu
+export default Menu;
--- Restaurants.js
+++ Restaurants.js
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from "react"
+import RenderDOM from 'react';
 import { Link, useParams } from 'react-router-dom';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Menu from './Menu';
 import './index.css';
 
 
 // research react-router
-
 
 
 const Restaurants = () => {
@@ -13,84 +14,24 @@ const Restaurants = () => {
 
   return meatList.map((restaurant) => {
     return (
-      <Link to="/menu">
+      <Link to={`/menu/${restaurant.store_name}`}>
         <div className="restaurant-container">
           <div className="image-container">
             <img src={restaurant.image} alt={restaurant.alt} />
           </div>
-          <div className="info-container">
+          <div className="info-container" >
             <h4>{restaurant.store_name}</h4>
             <h5>Pick-up</h5>
           </div>
         </div>
       </Link>
 
-
-
     );
-
-
-
   })
 }
-//   {meatList.map((restaurant) => {
-//     return (
-//       <>
-//         <h1>{restaurant.store_name}</h1>
-//         <div>
-//           <h2>Meat Options</h2>
-//           {restaurant.main_meats.map((meat) => {
-//             if (meat.name && meat.price) {
-//               return (
-//                 <div>
-//                   <h3>{meat.name}</h3>
-//                   <p>${meat.price}</p>
-//                 </div>
-//               );
-//             } else if (meat.name && meat.options) {
-//               return (
-//                 <div>
-//                   <h3>{meat.name}</h3>
-//                   {meat.options.map((option) => {
-//                     return (
-//                       <>
-//                         <h5>{option.name}</h5>
-//                         <p>${option.price}</p>
-//                       </>
-//                     )
-//                   })}
-//                 </div>
-//               )
-//             }
-//           }
-//           )}
-//         </div>
-//         <div>
-//           <h2>Available Add-Ons</h2>
-//           {restaurant.add_ons.map((addOnsOptions) => {
-//             if (addOnsOptions.name && addOnsOptions.price && !addOnsOptions.options) {
-//               return (
-//                 <>
-//                   <h3>{addOnsOptions.name}</h3>
-//                   <p>{addOnsOptions.price}</p>
-//                 </>
-//               );
-//             } else if (addOnsOptions.name && addOnsOptions.price && addOnsOptions.options) {
-//               return (
-//                 <>
-//                   <h3>{addOnsOptions.name}</h3>
-//                   <h5>Options: {addOnsOptions.options}</h5>
-//                   <p>{addOnsOptions.price}</p>
-//                 </>
-//               );
-//             }
-//           })}
-//         </div>
-//       </>
-//     );
-//   })}
-// </>
-
-
 
 export default Restaurants;
+
+
+
+
