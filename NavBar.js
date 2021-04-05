9895f1fc68b57314463bbd87abacbcf17fe481e8
Toriong <67920345+Toriong@users.noreply.github.com>
2021-04-04 22:46:46 -0500

    Added css to navbar and Restaurant component

--- NavBar.js
+++ NavBar.js
@@ -1,29 +1,61 @@
+import 'font-awesome/css/font-awesome.min.css'
+
+
+
 
 const NavBar = () => {
-    return <div className="navbar">
-        <div className="hamburger-menu-container">
-            <div className="hamburger-menu-sub-container">
-                <div id="hamburger-line-1" className="hamburger-line"></div>
-                <div id="hamburger-line-2" className="hamburger-line"></div>
-                <div id="hamburger-line-3" className="hamburger-line"></div>
+    return <div className="unfixed-wrapper">
+        <div className="navbar">
+            <div className="hamburger-menu-container">
+                <div className="hamburger-menu-sub-container">
+                    <div id="hamburger-line-1" className="hamburger-line"></div>
+                    <div id="hamburger-line-2" className="hamburger-line"></div>
+                    <div id="hamburger-line-3" className="hamburger-line"></div>
+                </div>
             </div>
-        </div>
-        <div className="logo-container">
-            <div id="quick-text">
-                Quick
+            <div className="logo-container">
+                <div id="quick-text">
+                    Quick
             </div>
-            <div id="carnivore-text">
-                Carnivore
+                <div id="carnivore-text">
+                    Carnivore
             </div>
-            <div id="eats-text">
-                Eats
+                <div id="eats-text">
+                    Eats
             </div>
-        </div>
-        <div className='deliver-or-pick-up'>
+            </div>
+            <div className='deliver-or-pick-up-container'>
+                <div className="deliver-or-pick-up-button">
+                    <div id="pick-up">
+                        Pick-up
+                </div>
+                    <div id="deliver">
+                        Deliver
+                </div>
+                </div>
+            </div>
+            <div className='search-bar-container'>
 
-        </div>
-        <div className='search-bar'>
-
+                <div className="search-bar-sub-container">
+                    <div id="search-icon-container">
+                        <i class="fa fa-search" aria-hidden="true"></i>
+                    </div>
+                    <input type="text" className="search-input" />
+                </div>
+            </div>
+            <div className="cart-container">
+                <div id="cart">
+                    <div className="cart-icon">
+                        <i class="fa fa-shopping-cart" aria-hidden="true"></i>
+                    </div>
+                    <div id="cart-text">
+                        Cart:
+                </div>
+                    <div id="number-of-items">
+                        0
+                </div>
+                </div>
+            </div>
         </div>
     </div>
 }
--- Restaurants.js
+++ Restaurants.js
@@ -3,41 +3,38 @@ import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom';
 
 
+
 // "react": "^17.0.2",
 //   "react-dom": "^17.0.2",
 
 
 
 const meatList = require('./Meat-Shops.json');
-// research react-router
 
 
 const Restaurants = () => {
-  useEffect(() => {
-    console.log(ReactDOM.version)
-  })
-  return meatList.map((restaurant) => {
-    return (
-      <div>
-        <Link to={`/menu/${restaurant.urlParams}`}>
-          <div className="restaurant-container">
-            <div className="image-container">
-              <img src={restaurant.image} alt={restaurant.alt} />
+  return <div className="homepage-wrapper">
+    <div className="restaurants-results-container">
+      {meatList.map((restaurant) => {
+        return <div className="restaurant-container">
+          <Link to={`/menu/${restaurant.urlParams}`} className="link-to-restaurant">
+            <div className="image-and-info-parent-container">
+              <div className="image-container">
+                <img src={restaurant.image} alt={restaurant.alt} />
+              </div>
+              <div className="info-container" >
+                <h4>{restaurant.domDisplayName}</h4>
+                <h5>Pick-up</h5>
+              </div>
             </div>
-            <div className="info-container" >
-              <h4>{restaurant.domDisplayName}</h4>
-              <h5>Pick-up</h5>
-            </div>
-          </div>
-        </Link>
-      </div>
-
-
-    );
-  })
+          </Link>
+        </div>
+      })}
+    </div>
+  </div>
 }
 
-
+// </Link >
 
 
 export default Restaurants;
