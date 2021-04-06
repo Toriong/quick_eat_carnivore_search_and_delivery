baa4944ef4a63884a17c9f3875d175298d6f23db
Toriong <67920345+Toriong@users.noreply.github.com>
2021-04-05 21:18:40 -0500

    more css styling added to Restaurants component
    added more CSS styling to the Restaurants component. All of the available restaurants near the user will appear on the left-hand side in a container on the UI. The list is scrollable.

--- Restaurants.js
+++ Restaurants.js
@@ -1,39 +1,25 @@
-import { BrowserRouter, Link } from 'react-router-dom';
 import React, { useEffect } from 'react';
-import ReactDOM from 'react-dom';
-
-
-
-// "react": "^17.0.2",
-//   "react-dom": "^17.0.2",
-
-
-
-const meatList = require('./Meat-Shops.json');
+import { BrowserRouter, Link } from 'react-router-dom';
+import meatList from './Meat-Shops.json'
 
 
 const Restaurants = () => {
-  return <div className="homepage-wrapper">
-    <div className="restaurants-results-container">
-      {meatList.map((restaurant) => {
-        return <div className="restaurant-container">
-          <Link to={`/menu/${restaurant.urlParams}`} className="link-to-restaurant">
-            <div className="image-and-info-parent-container">
-              <div className="image-container">
-                <img src={restaurant.image} alt={restaurant.alt} />
-              </div>
-              <div className="info-container" >
-                <h4>{restaurant.domDisplayName}</h4>
-                <h5>Pick-up</h5>
-              </div>
-            </div>
-          </Link>
+  return <div className="restaurants-list-container">
+    {meatList.map((restaurant) => {
+      return <Link to={`/menu/${restaurant.urlParams}`}>
+        <div className="restaurant-container">
+          <div className="image-container">
+            <img src={restaurant.image} alt={restaurant.alt} />
+          </div>
+          <div className="info-container">
+            <h5>{restaurant.domDisplayName}</h5>
+          </div>
         </div>
-      })}
-    </div>
+      </Link>
+    })
+    }
   </div>
 }
-
 // </Link >
 
 
